bd6552f94da6b870d61b5c645e05efe4bc4b026e
DESKTOP-DLOPF19\SSAFY <ajtwlssoqkd@naver.com>
2025-07-13 16:03:07 +0900

    statistic 수정

--- src/main/java/com/ssafy/statistic/repository/StatisticRepository.java
+++ src/main/java/com/ssafy/statistic/repository/StatisticRepository.java
@@ -4,7 +4,6 @@ import com.ssafy.transaction.entity.Transaction;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-
 import java.util.Date;
 import java.util.List;
 
@@ -12,14 +11,20 @@ import java.util.List;
 public interface StatisticRepository {
 
     // 1. 특정 기간, 사용자, (선택)카테고리의 지출(Expense) 거래 조회
-    @Select("""
-        SELECT * FROM transactions
-        WHERE user_id = #{user_id}
-          AND type = 'EXPENSE'
-          AND (#{category_ids} IS NULL OR category_id IN
-                <foreach collection='category_ids' item='id' open='(' separator=',' close=')'>#{id}</foreach>)
-          AND date BETWEEN #{start_date} AND #{end_date}
-    """)
+    @Select({
+        "<script>",
+        "SELECT * FROM transactions",
+        "WHERE user_id = #{user_id}",
+        "AND type = 'EXPENSE'",
+        "<if test='category_ids != null'>",
+        "AND category_id IN",
+        "<foreach collection='category_ids' item='id' open='(' separator=',' close=')'>",
+        "#{id}",
+        "</foreach>",
+        "</if>",
+        "AND date BETWEEN #{start_date} AND #{end_date}",
+        "</script>"
+    })
     List<Transaction> findExpensesByUserAndCategoriesAndDate(
         @Param("user_id") Long user_id,
         @Param("category_ids") List<Integer> category_ids,
@@ -28,13 +33,19 @@ public interface StatisticRepository {
     );
 
     // 2. 특정 기간, 사용자, (선택)카테고리의 전체 거래(INCOME/EXPENSE) 조회
-    @Select("""
-        SELECT * FROM transactions
-        WHERE user_id = #{user_id}
-          AND (#{category_ids} IS NULL OR category_id IN
-                <foreach collection='category_ids' item='id' open='(' separator=',' close=')'>#{id}</foreach>)
-          AND date BETWEEN #{start_date} AND #{end_date}
-    """)
+    @Select({
+        "<script>",
+        "SELECT * FROM transactions",
+        "WHERE user_id = #{user_id}",
+        "<if test='category_ids != null'>",
+        "AND category_id IN",
+        "<foreach collection='category_ids' item='id' open='(' separator=',' close=')'>",
+        "#{id}",
+        "</foreach>",
+        "</if>",
+        "AND date BETWEEN #{start_date} AND #{end_date}",
+        "</script>"
+    })
     List<Transaction> findTransactionsByUserAndCategoriesAndDate(
         @Param("user_id") Long user_id,
         @Param("category_ids") List<Integer> category_ids,
